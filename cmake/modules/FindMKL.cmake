83d26ea92d08f55eef63270a6af0f49d2f2c9857
Raul Laasner <raullaasner@gmail.com>
2019-10-12 20:29:26 -0400

    Remove copyright notices from the Find* modules

--- cmake/modules/FindMKL.cmake
+++ cmake/modules/FindMKL.cmake
@@ -1,8 +1,3 @@
-# This Source Code Form is subject to the terms of the GNU General
-# Public License, v. 3.0. If a copy of the GPL was not distributed
-# with this file, You can obtain one at
-# http://gnu.org/copyleft/gpl.txt.
-
 include(FindPackageHandleStandardArgs)
 
 if (DEFINED ENV{MKLROOT})
