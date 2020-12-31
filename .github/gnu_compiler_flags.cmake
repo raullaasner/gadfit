a8e3d14fa2775f7baf5a44a18a08dbce652eb32a
Raul Laasner <raullaasner@gmail.com>
2020-12-31 17:27:49 +0100

    Add a workflow

---
+++ .github/gnu_compiler_flags.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_CXX_FLAGS_RELEASE "-O3 -march=native -DNDEBUG" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_DEBUG "-Og -g -Wall -Wextra -Wcast-align -Wformat -Winvalid-pch -Wmissing-declarations -Wmissing-include-dirs -Wconversion -Wredundant-decls -Wswitch-default -Wswitch-enum -pedantic" CACHE STRING "")
+set(CMAKE_Fortran_FLAGS "-ffree-line-length-0" CACHE STRING "")
+set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -funroll-all-loops -fcoarray=lib" CACHE STRING "")
+set(CMAKE_Fortran_FLAGS_DEBUG "-O3 -g -Wall -Wno-maybe-uninitialized -Wextra -pedantic -fcheck=all -ffpe-trap=zero,overflow -fbacktrace -fcoarray=lib -std=f2008ts" CACHE STRING "")
